2da6cb7f6edfb277486b8cf7c19cc7948743e034
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-03-27 20:42:05 +0100

    overflow hidden

--- components/Homepage/Homepage.tsx
+++ components/Homepage/Homepage.tsx
@@ -6,7 +6,7 @@ import Contact from './sections/Contact'
 
 const Homepage = () => {
   return (
-    <div className=' h-[100vh] overflow-y-scroll scroll-smooth pb-[20px]'>
+    <div className=' h-[100vh] overflow-y-scroll scroll-smooth pb-[20px] overflow-x-hidden'>
       <Hero/>
       <About/>
       <Services/>
